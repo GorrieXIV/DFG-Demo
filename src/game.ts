5373ccea85bb1c3092c23941b3a49021519da1ed
Kurtis Trainor <ktrainor@doc-it.com>
2020-03-07 14:19:43 -0500

    tweaked game.ts

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,48 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
+export default class TitleScreen extends Phaser.Scene
 {
     constructor ()
     {
-        super('demo');
+        super('TitleScreen');
     }
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        // this.load.image('logo', 'assets/phaser3-logo.png');
+        // this.load.image('libs', 'assets/libs.png');
+        // this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
+        // this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+        // this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
 
-        this.add.image(400, 300, 'libs');
+        // this.add.image(400, 300, 'libs');
 
-        const logo = this.add.image(400, 70, 'logo');
+        // const logo = this.add.image(400, 70, 'logo');
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 350,
+        //     duration: 1500,
+        //     ease: 'Sine.inOut',
+        //     yoyo: true,
+        //     repeat: -1
+        // })
     }
+
 }
 
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+    backgroundColor: '#FFFFFF',
+    width: 1080,
+    height: 1920,
+    scene: TitleScreen
 };
 
 const game = new Phaser.Game(config);
