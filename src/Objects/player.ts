3808a139567514e4ad3ef205a8df2aeda11e5edb
Kurtis Trainor <ktrainor@doc-it.com>
2020-03-07 20:31:20 -0500

    changed fightya

--- src/Objects/player.ts
+++ src/Objects/player.ts
@@ -3,6 +3,6 @@ import { Scene } from '../Scenes/base';
 
 export class PlayerCharacter extends GameObject {
     constructor(scene: Scene, x: number, y: number) {
-        super(scene, 'player', x, y);
+        super(scene, 'fighter', x, y);
     }
 }
