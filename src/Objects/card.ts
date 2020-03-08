f925236717841f79de387257c1f5e8a4684e4f92
Kurtis Trainor <ktrainor@doc-it.com>
2020-03-07 22:08:37 -0500

    card constructors

--- src/Objects/card.ts
+++ src/Objects/card.ts
@@ -1,6 +1,5 @@
 import { GameObject } from './base';
 import { Scene } from '../Scenes/base';
-import { randomIntFromRange } from '../Math'
 
 export enum CardType
 {
@@ -13,45 +12,44 @@ export class Card extends GameObject {
     damage:   number;
     cooldown: number;
 
+    constructor(scene: Scene, type: string, x: number, y: number) {
+        super(scene, type, x, y);
+    }
+
+}
+
+export class WeakAttackCard extends Card {
+
     constructor(scene: Scene, x: number, y: number) {
-        super(scene, 'card', x, y);
-        let random = randomIntFromRange(1, 3);
-        switch (random)
-        {
-            case 1:
-                this.setGrab();
-                break;
-            case 2:
-                this.setWeakAttack();
-                break;
-            case 3:
-                this.setStrongAttack();
-                break;
-            
-        }
-    }
-
-    setGrab()
-    {
-        this.title = "Grab";
-        this.cardType = CardType.Attack;
-        this.damage = 1;
-        this.cooldown = 1;
-    }
-
-    setWeakAttack()
-    {
+        super(scene, 'weakattackcard', x, y);
         this.title = "Weak Attack";
         this.cardType = CardType.Attack;
         this.damage = 2;
         this.cooldown = 2;
     }
 
-    setStrongAttack()
-    {
+}
+
+export class StrongAttackCard extends Card {
+
+    constructor(scene: Scene, x: number, y: number) {
+        super(scene, 'strongattackcard', x, y);
         this.title = "Strong Attack";
         this.cardType = CardType.Attack;
         this.damage = 3;
         this.cooldown = 3;
     }
+
+}
+
+export class GrabCard extends Card {
+
+    constructor(scene: Scene, x: number, y: number) {
+        super(scene, 'grabcard', x, y);
+        this.title = "Grab";
+        this.cardType = CardType.Attack;
+        this.damage = 1;
+        this.cooldown = 1;
+    }
+
 }
